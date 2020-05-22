f492320256dc5ed0b8ed48cfba3855ca4a82ae56
Kevin Cantrell <cantrell@up.edu>
2020-05-21 21:36:22 -0700

    more functions

--- chemicalVision_processFrame_Functions.py
+++ chemicalVision_processFrame_Functions.py
@@ -94,7 +94,6 @@ def WhiteBalanceFrame(rotImage,frame,dictSet):
         bscale=RGBGreyWBR[0]
         gscale=RGBGreyWBR[1]
         rscale=RGBGreyWBR[2]
-        scalemax=max(rscale,gscale,bscale)
         if dictSet['WBR sc'][0]!=0:
             scalemin=dictSet['WBR sc'][0]
         else:
@@ -110,6 +109,40 @@ def WhiteBalanceFrame(rotImage,frame,dictSet):
     rgbWBR[dictSet['WB3 xy'][1]:dictSet['WB3 xy'][1]+dictSet['WB3 wh'][1], dictSet['WB3 xy'][0]:dictSet['WB3 xy'][0]+dictSet['WB3 wh'][0]] = rotImage[dictSet['WB3 xy'][1]:dictSet['WB3 xy'][1]+dictSet['WB3 wh'][1], dictSet['WB3 xy'][0]:dictSet['WB3 xy'][0]+dictSet['WB3 wh'][0]]
     return(rgbWBR,rotImage,frame)
 
+def SummarizeROI(rotImage,roiSetName,dictSet):
+    rgbROI = rotImage[dictSet[roiSetName+' xy'][1]:dictSet[roiSetName+' xy'][1]+dictSet[roiSetName+' wh'][1], dictSet[roiSetName+' xy'][0]:dictSet[roiSetName+' xy'][0]+dictSet[roiSetName+' wh'][0]]
+    hsvROI = cv2.cvtColor(rgbROI, cv2.COLOR_BGR2HSV)
+    hsvROI[:,:,0]=ip.ShiftHOriginToValue(hsvROI[:,:,0],dictSet['hue lo'][0],dictSet['hue lo'][1])
+    labROI = cv2.cvtColor(rgbROI, cv2.COLOR_BGR2LAB)
+    logsrgbROI=cv2.LUT(rgbROI, linLUTabs)*64
+    maskROI = cv2.inRange(hsvROI, np.array(dictSet[roiSetName+' ll']), np.array(dictSet[roiSetName+' ul']))
+    rgbROIsummary=cv2.meanStdDev(rgbROI,mask=maskROI)
+
+    contourROI=FindLargestContour(maskROI)
+    
+    if float(float(cv2.__version__[0])+float(cv2.__version__[2])/10)>=4:
+        contours,hierarchy = cv2.findContours(maskRO1,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    else:
+        image,contours,hierarchy = cv2.findContours(maskRO1,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    contourMask=np.zeros((maskRO1.shape),dtype='uint8')
+    if len(contours)>=1:
+        MaxRO1Area=0
+        ContourIndex=0
+        LargestContour=0
+        for contour in contours:
+            area=cv2.contourArea(contour)
+            if area>MaxRO1Area:
+                MaxRO1Area=area
+                LargestContour=ContourIndex
+            ContourIndex=ContourIndex+1
+        outerRO1Contour=contours[LargestContour]
+        boundingRectangle=cv2.minAreaRect(outerRO1Contour)
+        #cv2.drawContours(resFrameRO1,[outerRO1Contour],0,(0,255,0),2)
+        cv2.drawContours(contourMask,[outerRO1Contour],0,(255),-1)
+        resMask = cv2.bitwise_and(maskRO1,maskRO1, mask= contourMask)
+        resFrameRO1 = cv2.bitwise_and(rgbRO1,rgbRO1, mask= resMask)
+
+
 
 
 
@@ -118,14 +151,23 @@ def WhiteBalanceFrame(rotImage,frame,dictSet):
 
     
 cap = cv2.VideoCapture(video_file_path)
+TotalFrames=int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+ParameterStats=np.zeros((32,6,TotalFrames*2,5))
+#ParameterStats Map
+#1st dimension 0 to 14: labels=["R","G","B","H","S","V","L","a","b","Ra","Ga","Ba","Ga-Ra","Ba-Ra","Ga-Ba"]
+#1st dimension 15 to 17: labels=["RO1area","RO1BoundingRecHeight","RO1BoundingRecWidth"
+#1st dimension 29 to 31: labels=["frameRate","frameNumber","frameTime"]
+#2nd dimension 0 to 5: labels=["mean","stdev","dominant"]
+#3rd dimension frame number
+#4th dimension 0 to 4: labels=["WBR","RO1","RO2","RO3"]
 ret, frame = cap.read()    
+
 def ProcessOneFrame(frame,dictSet):    
     displayWidth=dictSet['dsp wh'][0]
     displayHeight=dictSet['dsp wh'][1]
     flgFindReference=dictSet['flg rf'][0]
     displayFrame = np.zeros((displayHeight, displayWidth, 3), np.uint8)
     img=np.copy(frame)
-    
     if flgFindReference:
         rotImage = RegisterImage(frame,img,dictSet)
         skipFrame=False
@@ -134,31 +176,38 @@ def ProcessOneFrame(frame,dictSet):
     else:
         rotImage = np.copy(frame)
         skipFrame=False
-   
     if skipFrame==False:
-        # Working here: Idea is to put multiple rectangular regions into rgbWBR
         rgbWBR,rotImage,frame=WhiteBalanceFrame(rotImage,frame,dictSet)
         hsvWBR = cv2.cvtColor(rgbWBR, cv2.COLOR_BGR2HSV)
         maskWBR = cv2.inRange(hsvWBR, np.array(dictSet['WBR ll']), np.array(dictSet['WBR ul']))
+        rgbWBRsummary=cv2.meanStdDev(rgbWBR,mask=maskWBR)
         for row, displayColor, channel in zip([0,1,2], [(0, 0, 128),(0, 128, 0),(128, 25, 25)], [2,1,0]):              
-            #ParameterStats[row,0,frameNumber,1],ParameterStats[row,1,frameNumber,1],ParameterStats[row,2,frameNumber,1]=OpenCVDisplayedHistogram(inputImage,channel,maskRO1,256,0,255,displayWidth/2,5+(row*displayHeight/10)+(row*5),256,(displayHeight/12),displayFrame,displayColor,5,True,label)
             ParameterStats[row,0,frameNumber,0],ParameterStats[row,1,frameNumber,0],ParameterStats[row,2,frameNumber,0]=ip.OpenCVDisplayedHistogram(rgbWBR,channel,maskWBR,256,0,255,displayWidth/2,5+(row*displayHeight/14)+(row*6),256,(displayHeight/16),displayFrame,displayColor,5,False)
-        rgbRO2 = rotImage[dictSet['RO2 xy'][1]:dictSet['RO2 xy'][1]+dictSet['RO2 wh'][1], dictSet['RO2 xy'][0]:dictSet['RO2 xy'][0]+dictSet['RO2 wh'][0]]
-        rgbRO3 = rotImage[dictSet['RO3 xy'][1]:dictSet['RO3 xy'][1]+dictSet['RO3 wh'][1], dictSet['RO3 xy'][0]:dictSet['RO3 xy'][0]+dictSet['RO3 wh'][0]]
+        resFrameWBR = cv2.bitwise_and(rgbWBR,rgbWBR, mask= maskWBR)
+
         rgbRO1 = rotImage[dictSet['RO1 xy'][1]:dictSet['RO1 xy'][1]+dictSet['RO1 wh'][1], dictSet['RO1 xy'][0]:dictSet['RO1 xy'][0]+dictSet['RO1 wh'][0]]
-        rgbRO2summary=cv2.meanStdDev(rgbRO2)
-        rgbRO3summary=cv2.meanStdDev(rgbRO3)
-
         hsvRO1 = cv2.cvtColor(rgbRO1, cv2.COLOR_BGR2HSV)
-
         hsvRO1[:,:,0]=ip.ShiftHOriginToValue(hsvRO1[:,:,0],dictSet['hue lo'][0],dictSet['hue lo'][1])
-
         labRO1 = cv2.cvtColor(rgbRO1, cv2.COLOR_BGR2LAB)
         logsrgbRO1=cv2.LUT(rgbRO1, linLUTabs)*64
         maskRO1 = cv2.inRange(hsvRO1, np.array(dictSet['RO1 ll']), np.array(dictSet['RO1 ul']))
-        #maskRO2 = cv2.inRange(hsvRO2, np.array(dictSet['RO2 ll']), np.array(dictSet['RO2 ul']))
-        #maskRO3 = cv2.inRange(hsvRO3, np.array(dictSet['RO3 ll']), np.array(dictSet['RO3 ul']))
-        resFrameWBR = cv2.bitwise_and(rgbWBR,rgbWBR, mask= maskWBR)
+        rgbRO1summary=cv2.meanStdDev(rgbRO1,mask=maskRO1)
+
+        rgbRO2 = rotImage[dictSet['RO2 xy'][1]:dictSet['RO2 xy'][1]+dictSet['RO2 wh'][1], dictSet['RO2 xy'][0]:dictSet['RO2 xy'][0]+dictSet['RO2 wh'][0]]
+        hsvRO2 = cv2.cvtColor(rgbRO2, cv2.COLOR_BGR2HSV)
+        hsvRO2[:,:,0]=ip.ShiftHOriginToValue(hsvRO2[:,:,0],dictSet['hue lo'][0],dictSet['hue lo'][1])
+        labRO2 = cv2.cvtColor(rgbRO2, cv2.COLOR_BGR2LAB)
+        logsrgbRO2=cv2.LUT(rgbRO2, linLUTabs)*64
+        maskRO2 = cv2.inRange(hsvRO2, np.array(dictSet['RO2 ll']), np.array(dictSet['RO2 ul']))
+        rgbRO2summary=cv2.meanStdDev(rgbRO2,mask=maskRO2)
+
+        rgbRO3 = rotImage[dictSet['RO3 xy'][1]:dictSet['RO3 xy'][1]+dictSet['RO3 wh'][1], dictSet['RO3 xy'][0]:dictSet['RO3 xy'][0]+dictSet['RO3 wh'][0]]
+        hsvRO3 = cv2.cvtColor(rgbRO3, cv2.COLOR_BGR2HSV)
+        hsvRO3[:,:,0]=ip.ShiftHOriginToValue(hsvRO2[:,:,0],dictSet['hue lo'][0],dictSet['hue lo'][1])
+        labRO3 = cv2.cvtColor(rgbRO3, cv2.COLOR_BGR2LAB)
+        logsrgbRO3=cv2.LUT(rgbRO3, linLUTabs)*64
+        maskRO3 = cv2.inRange(hsvRO3, np.array(dictSet['RO3 ll']), np.array(dictSet['RO3 ul']))
+        rgbRO3summary=cv2.meanStdDev(rgbRO3,mask=maskRO3)
 
         if float(float(cv2.__version__[0])+float(cv2.__version__[2])/10)>=4:
             contours,hierarchy = cv2.findContours(maskRO1,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
@@ -201,9 +250,9 @@ def ProcessOneFrame(frame,dictSet):
             ParameterStats[29,0,frameNumber,:]=frameRate
             #Analytical signal should be in channel 13 of parameter stats B absorbance -R absorbance for I2
             labels=["R","G","B","H","S","V","L","a","b","Ra","Ga","Ba","Ga-Ra","Ba-Ra","Ga-Ba"]
-            ParameterStats[12,0,frameNumber,:]=ParameterStats[10,0,frameNumber,:]-ParameterStats[9,0,frameNumber,:]
-            ParameterStats[13,0,frameNumber,:]=ParameterStats[11,0,frameNumber,:]-ParameterStats[9,0,frameNumber,:]
-            ParameterStats[14,0,frameNumber,:]=ParameterStats[10,0,frameNumber,:]-ParameterStats[11,0,frameNumber,:]
+            #ParameterStats[12,0,frameNumber,:]=ParameterStats[10,0,frameNumber,:]-ParameterStats[9,0,frameNumber,:]
+            #ParameterStats[13,0,frameNumber,:]=ParameterStats[11,0,frameNumber,:]-ParameterStats[9,0,frameNumber,:]
+            #ParameterStats[14,0,frameNumber,:]=ParameterStats[10,0,frameNumber,:]-ParameterStats[11,0,frameNumber,:]
             
             RO1Scale=max(resFrameRO1.shape[1]/(displayWidth/4),resFrameRO1.shape[0]/(displayHeight/4))
             RO1ImageScale = cv2.resize(resFrameRO1, (int(resFrameRO1.shape[1]/RO1Scale),int(resFrameRO1.shape[0]/RO1Scale)), interpolation = cv2.INTER_AREA)
@@ -226,24 +275,7 @@ def ProcessOneFrame(frame,dictSet):
             ParameterStats[12,0,frameNumber,1]=ParameterStats[10,0,frameNumber,1]-ParameterStats[9,0,frameNumber,1]
             ParameterStats[13,0,frameNumber,1]=ParameterStats[11,0,frameNumber,1]-ParameterStats[9,0,frameNumber,1]
             ParameterStats[14,0,frameNumber,1]=ParameterStats[10,0,frameNumber,1]-ParameterStats[11,0,frameNumber,1]
-            maskRO1Volume = cv2.inRange(hsvRO1, np.array([int(ParameterStats[3,0,frameNumber,1]-ParameterStats[3,1,frameNumber,1]),int(ParameterStats[4,0,frameNumber,1]-ParameterStats[4,1,frameNumber,1]),int(ParameterStats[5,0,frameNumber,1]-ParameterStats[5,1,frameNumber,1])]), np.array([255,255,255]))
-            if float(float(cv2.__version__[0])+float(cv2.__version__[2])/10)>=4:
-                contours,hierarchy = cv2.findContours(maskRO1Volume,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-            else:
-                image,contours,hierarchy = cv2.findContours(maskRO1Volume,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-            contourMask=np.zeros((maskRO1Volume.shape),dtype='uint8')
-            if len(contours)>=1:
-                MaxRO1AreaVolume=0
-                ContourIndex=0
-                LargestContour=0
-                for contour in contours:
-                    area=cv2.contourArea(contour)
-                    if area>MaxRO1AreaVolume:
-                        MaxRO1AreaVolume=area
-                        LargestContour=ContourIndex
-                    ContourIndex=ContourIndex+1
-                outerRO1ContourVolume=contours[LargestContour]
-            ParameterStats[18,0,frameNumber,1]=MaxRO1AreaVolume*cmPerPixel*cmPerPixel
+
             if frameNumber>=2:
                 if dictSet['a1x sc'][0]==0:
                     xMin=dictSet['a1x sc'][1]
